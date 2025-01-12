740d53fad9691b0e56abad8a677aad3a4384e0b5
tirtha9live <tirtha9@live.in>
2025-01-12 15:06:55 +0530

    changed appname to restaurant

--- restaurant/migrations/0002_menu_menu_item_description.py
+++ restaurant/migrations/0002_menu_menu_item_description.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('resturant', '0001_initial'),
+        ('restaurant', '0001_initial'),
     ]
 
     operations = [
--- restaurant/migrations/0003_remove_booking_comment_remove_booking_guest_number_and_more.py
+++ restaurant/migrations/0003_remove_booking_comment_remove_booking_guest_number_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('resturant', '0002_menu_menu_item_description'),
+        ('restaurant', '0002_menu_menu_item_description'),
     ]
 
     operations = [
